bea794ef85ff165cc452525c3af9614bbed3b5d9
umniknik <kaaaaaljan@mail.ru>
2024-08-03 08:30:38 +0300

    ДЗ к занятию "Компоненты - Сделана 2 часть (class-based компонент).

---
+++ src/ShopItemClass.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default class ShopItemClass extends React.Component {
+    render() {
+
+        const {item} = this.props;  //Записываем переданные свойства props в переменную. Используем деструктуризацию, чтобы обращаться к свойствам item.brand а не item.item.brand
+
+        return (
+            <div className='main-content'>
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className='description'>{item.descriptionFull}</div>
+            <div className='highlight-window mobile'><div className='highlight-overlay'></div></div>
+            <div className='divider'></div>
+            <div className='purchase-info'>
+              <div className='price'>{item.currency + item.price.toFixed(2)}</div>
+              <button>Добавить в корзину</button>
+            </div>
+          </div>
+        )
+    }
+}
